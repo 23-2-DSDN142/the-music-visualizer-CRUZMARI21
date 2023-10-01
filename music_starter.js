3ab8168336b40e5be6dbdccab9dad2401efa4100
CRUZMARI21 <140350193+CRUZMARI21@users.noreply.github.com>
2023-10-01 15:25:34 +1300

    Maria started drawing landscape - insert sun, clouds and road

--- music_starter.js
+++ music_starter.js
@@ -4,14 +4,40 @@
 
 function draw_one_frame(vocal, drum, bass, other) {
 colorMode(HSB, 100);
-background (255)
-rectMode (CENTER)
+background('#7BD5DB')
+rectMode(CENTER)
 strokeWeight(9)
 stroke(drum, 80,80);
 
+//Draw Road Side 
+fill('black')
+rect(270, 500,550,450);
+let rectX = 50;
+let fr = 200; //starting FPS
+let clr;
+clr = color(255, 50, 0);
+
+//Draw Sun 
+fill('#FFF05C')
+ellipse(250,140,250,240)
+
+
+//Draw Clouds
+fill('#66ACDB')
+stroke('#66ACDB')
+ellipse(20,60,70,10)//left
+ellipse(55,50,60,10)//middle
+ellipse(90,60,70,10)//right
+
+fill('#66ACDB')
+ellipse(34,160,50,10)//left
+ellipse(60,150,50,10)//middle
+ellipse(90,160,50,10)//right
+
+//Road 
 let drumMap = map(drum, 0,100,5,70); //parseint
-let lengthOfLine = 400;
-let LineStart = 100;
+let lengthOfLine = 100;
+let LineStart = 10;
 let lineEnd = LineStart+lengthOfLine;
 
 line(LineStart, 300, lineEnd, 300); 
@@ -28,7 +54,14 @@ ellipse(width/2, drumHight, ballSize/2);
 }
 
 
+
+
+
 }
+
+
+
+
 
 
 
