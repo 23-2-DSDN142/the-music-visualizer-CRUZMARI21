7e0dcb92e02bef9782b97decbcc931b7c69cdd55
CRUZMARI21 <140350193+CRUZMARI21@users.noreply.github.com>
2023-10-02 10:36:53 +1300

    Loaded palm tree image

--- music_starter.js
+++ music_starter.js
@@ -1,5 +1,7 @@
-
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
+let firstRun = true 
+let PalmTrees; 
+
 
 
 function draw_one_frame(vocal, drum, bass, other) {
@@ -9,53 +11,77 @@ rectMode(CENTER)
 strokeWeight(9)
 stroke(drum, 80,80);
 
+if(firstRun){ 
+rectMode(CENTER)
+PalmTrees = loadImage ('Palm Trees.png');
+firstRun = false 
+}
+
+
+
+//Draw Waves
+fill('#3DABF9')
+ellipse(300, 490, 850,90)
+
 //Draw Road Side 
 fill('black')
-rect(270, 500,550,450);
+rect(170, 655, 410, 600);
 let rectX = 50;
 let fr = 200; //starting FPS
 let clr;
 clr = color(255, 50, 0);
 
+
 //Draw Sun 
 fill('#FFF05C')
-ellipse(250,140,250,240)
+ellipse(265,140,250,240) 
 
+//Draw Clouds 
+fill('white')
+stroke('white')
+ellipse(34,60,50,10)//left
+ellipse(60,50,50,10)//middle
+ellipse(90,60,50,10)//right
 
-//Draw Clouds
-fill('#66ACDB')
-stroke('#66ACDB')
-ellipse(20,60,70,10)//left
-ellipse(55,50,60,10)//middle
-ellipse(90,60,70,10)//right
-
-fill('#66ACDB')
+fill('white')
+stroke('white')
 ellipse(34,160,50,10)//left
 ellipse(60,150,50,10)//middle
 ellipse(90,160,50,10)//right
 
-//Road 
+fill('white')
+stroke('white')
+ellipse(434,160,50,10)//left
+ellipse(460,150,50,10)//middle
+ellipse(485,160,50,10)//right
+
+
+//Road Line 
 let drumMap = map(drum, 0,100,5,70); //parseint
 let lengthOfLine = 100;
 let LineStart = 10;
 let lineEnd = LineStart+lengthOfLine;
 
-line(LineStart, 300, lineEnd, 300); 
-line(LineStart, 310, lineEnd, 310);
-line(LineStart, 320, lineEnd, 320);
+line(LineStart, 550, lineEnd, 550); 
+line(LineStart, 560, lineEnd, 560);
+line(LineStart, 570, lineEnd, 570);
+
 
 for(let i = 1; i < drumMap; i=i+4) {
 let lineStep = 1+20;
 line(LineStart, lineEnd, lineStep);
 
-let ballSize = map(bass, 15, 50, 40, 150) //ellipsebouncing 
-let drumHight = map(drum, 0 , 50, height, 3+ballSize); 
-ellipse(width/2, drumHight, ballSize/2);
+
+
+
+
 }
 
+let ballSize = map(bass, 25, 50, 40, 150) 
+let drumHight = map(drum, 0 , 50, height, 3+ballSize); 
+ellipse(width/2, drumHight, ballSize/2);
 
-
-
+image(PalmTrees, 50,-580)
 
 }
 
