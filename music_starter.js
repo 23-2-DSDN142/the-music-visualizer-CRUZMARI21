54a5ce19516521320e344d3824b670163e26c48e
CRUZMARI21 <140350193+CRUZMARI21@users.noreply.github.com>
2023-10-16 12:56:09 +1300

    push

--- music_starter.js
+++ music_starter.js
@@ -2,8 +2,6 @@
 let firstRun = true 
 let PalmTrees; 
 
-
-
 function draw_one_frame(vocal, drum, bass, other) {
 colorMode(HSB, 100);
 background('#7BD5DB')
@@ -11,96 +9,70 @@ rectMode(CENTER)
 strokeWeight(9)
 stroke(drum, 80,80);
 
-if(firstRun){ 
+if(firstRun){
 rectMode(CENTER)
 PalmTrees = loadImage ('Palm Trees.png');
-firstRun = false 
-}
+ firstRun = false
 
-
-
-//Draw Waves
+//Waves Drawing
 fill('#3DABF9')
 ellipse(300, 490, 850,90)
-
-//Draw Road Side 
-fill('black')
-rect(170, 655, 410, 600);
-let rectX = 50;
-let fr = 200; //starting FPS
-let clr;
-clr = color(255, 50, 0);
-
-
-//Draw Sun 
+    
+//Sun Drawing 
 fill('#FFF05C')
 ellipse(265,140,250,240) 
-
-//Draw Clouds 
+    
+//Clouds Drawing
 fill('white')
 stroke('white')
 ellipse(34,60,50,10)//left
 ellipse(60,50,50,10)//middle
 ellipse(90,60,50,10)//right
-
+    
 fill('white')
 stroke('white')
 ellipse(34,160,50,10)//left
 ellipse(60,150,50,10)//middle
 ellipse(90,160,50,10)//right
-
+    
 fill('white')
 stroke('white')
 ellipse(434,160,50,10)//left
 ellipse(460,150,50,10)//middle
 ellipse(485,160,50,10)//right
-
-
-//Road Line 
+    
+//Road Marks Drawing 
 let drumMap = map(drum, 0,100,5,70); //parseint
 let lengthOfLine = 100;
 let LineStart = 10;
 let lineEnd = LineStart+lengthOfLine;
-
+    
 line(LineStart, 550, lineEnd, 550); 
 line(LineStart, 560, lineEnd, 560);
 line(LineStart, 570, lineEnd, 570);
 
-
-for(let i = 1; i < drumMap; i=i+4) {
+}
+     
+    
+for(let i = 1; i < drumMap; i=i+4); {
 let lineStep = 1+20;
 line(LineStart, lineEnd, lineStep);
+    
+beginShape(TRIANGLES);
+fill('#000000')
+noStroke()
+vertex(-50, 420);
+vertex(40, 380);
+vertex(50, 280);
+vertex(160, 220);
+vertex(70, 275);
+endShape();
 
-
-
-
-
-}
+} 
 
 let ballSize = map(bass, 25, 50, 40, 150) 
 let drumHight = map(drum, 0 , 50, height, 3+ballSize); 
 ellipse(width/2, drumHight, ballSize/2);
 
-image(PalmTrees, 50,-580)
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+image(PalmTrees, 50,-580);
 
